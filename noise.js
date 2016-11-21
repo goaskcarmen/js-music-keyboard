0e9044bd25e486ebe2abef69f50e0567f5a789f6
Carmen Chan <carmenkellychan@gmail.com>
2016-11-21 15:18:17 -0800

    basic requirements

--- noise.js
+++ noise.js
@@ -1,3 +1,60 @@
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', 'button', function(event) {
+    switch ($(this).html()){
+      case 'c':
+        document.getElementById('cAudio').play();
+        break;
+      case 'd':
+        document.getElementById('dAudio').play();
+        break;
+      case 'e':
+        document.getElementById('eAudio').play();
+        break;
+      case 'f':
+        document.getElementById('fAudio').play();
+        break;
+      case 'g':
+        document.getElementById('gAudio').play();
+        break;
+      case 'a':
+        document.getElementById('aAudio').play();
+        break;
+      case 'b':
+        document.getElementById('bAudio').play();
+        break;
+      default:
+        //code
+
+    }
+  });
+
+  $('body').keydown(function(event){
+    switch (event.key)
+    {
+      case 'c':
+        document.getElementById('cAudio').play();
+        break;
+      case 'd':
+        document.getElementById('dAudio').play();
+        break;
+      case 'e':
+        document.getElementById('eAudio').play();
+        break;
+      case 'f':
+        document.getElementById('fAudio').play();
+        break;
+      case 'g':
+        document.getElementById('gAudio').play();
+        break;
+      case 'a':
+        document.getElementById('aAudio').play();
+        break;
+      case 'b':
+        document.getElementById('bAudio').play();
+        break;
+      default:
+        alert("no sound for this key.");
+    }
+
+  });
 });
